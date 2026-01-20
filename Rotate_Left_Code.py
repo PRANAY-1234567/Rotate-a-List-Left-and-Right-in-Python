e24e4c83461de31d4e766b3eef0ac0c7c3dd729c
Pranay <pranayvjadhao197@gmail.com>
2026-01-20 22:23:37 +0530

    Initial commit

---
+++ Rotate_Left_Code.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4]
+
+first = numbers.pop(0)
+numbers.append(first)
+
+print(numbers)
---
+++ Rotate_Right_Code.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4]
+
+last = numbers.pop()
+numbers.insert(0, last)
+
+print(numbers)
